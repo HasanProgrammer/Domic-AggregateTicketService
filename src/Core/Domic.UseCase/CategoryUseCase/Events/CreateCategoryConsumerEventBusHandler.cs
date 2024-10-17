c799e249b449ac2acac156a4d0274671eb739cd6
Hasan Karami <hasankarami2020313@gmail.com>
2024-10-17 22:49:05 +0330

    changes

---
+++ src/Core/Domic.UseCase/CategoryUseCase/Events/CreateCategoryConsumerEventBusHandler.cs
@@ -0,0 +1,34 @@
+﻿using Domic.Core.Common.ClassConsts;
+using Domic.Core.Domain.Enumerations;
+using Domic.Core.UseCase.Attributes;
+using Domic.Core.UseCase.Contracts.Interfaces;
+using Domic.Domain.Category.Events;
+using Domic.Domain.Ticket.Contracts.Interfaces;
+
+namespace Domic.UseCase.UserUseCase.Events;
+
+public class CreateCategoryConsumerEventBusHandler(ITicketQueryRepository ticketQueryRepository) 
+    : IConsumerEventBusHandler<CategoryDeleted>
+{
+    public void Handle(CategoryDeleted @event){}
+
+    [TransactionConfig(Type = TransactionType.Query)]
+    public async Task HandleAsync(CategoryDeleted @event, CancellationToken cancellationToken)
+    {
+        var tickets = await ticketQueryRepository.FindByCategoryIdAsync(@event.Id, cancellationToken);
+
+        if (tickets.Any())
+        {
+            foreach (var targetTicket in tickets)
+            {
+                targetTicket.IsDeleted = IsDeleted.Delete;
+                targetTicket.UpdatedBy = @event.UpdatedBy;
+                targetTicket.UpdatedRole = @event.UpdatedRole;
+                targetTicket.UpdatedAt_EnglishDate = @event.UpdatedAt_EnglishDate;
+                targetTicket.UpdatedAt_PersianDate = @event.UpdatedAt_PersianDate;
+            }
+            
+            ticketQueryRepository.ChangeRange(tickets);
+        }
+    }
+}
---
+++ src/Core/Domic.UseCase/UserUseCase/Events/CreateUserConsumerEventBusHandler.cs
@@ -0,0 +1,37 @@
+﻿using Domic.Core.Common.ClassConsts;
+using Domic.Core.Domain.Enumerations;
+using Domic.Core.UseCase.Attributes;
+using Domic.Core.UseCase.Contracts.Interfaces;
+using Domic.Domain.Service.Events;
+using Domic.Domain.Ticket.Contracts.Interfaces;
+
+namespace Domic.UseCase.UserUseCase.Events;
+
+public class CreateUserConsumerEventBusHandler(ITicketQueryRepository ticketQueryRepository) 
+    : IConsumerEventBusHandler<UserActived>
+{
+    public void Handle(UserActived @event){}
+
+    [TransactionConfig(Type = TransactionType.Query)]
+    public async Task HandleAsync(UserActived @event, CancellationToken cancellationToken)
+    {
+        var tickets =
+            await ticketQueryRepository.FindByUserIdConditionallyAsync(@event.Id,
+                ticket => ticket.IsActive == IsActive.InActive, cancellationToken
+            );
+
+        if (tickets.Any())
+        {
+            foreach (var ticket in tickets)
+            {
+                ticket.IsActive = IsActive.Active;
+                ticket.UpdatedBy = @event.UpdatedBy;
+                ticket.UpdatedRole = @event.UpdatedRole;
+                ticket.UpdatedAt_EnglishDate = @event.UpdatedAt_EnglishDate;
+                ticket.UpdatedAt_PersianDate = @event.UpdatedAt_PersianDate;
+            }
+            
+            ticketQueryRepository.ChangeRange(tickets);
+        }
+    }
+}
