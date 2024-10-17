7f3280be72a29949dffbc721e8adbeb716eb4b06
Hasan Karami <hasankarami2020313@gmail.com>
2024-10-17 19:05:06 +0330

    changes

--- src/Core/Domic.UseCase/TicketUseCase/Queries/ReadOne/ReadOneQueryHandler.cs
+++ src/Core/Domic.UseCase/TicketUseCase/Queries/ReadOne/ReadOneQueryHandler.cs
@@ -7,16 +7,20 @@ namespace Domic.UseCase.TicketUseCase.Queries.ReadOne;
 public class ReadOneQueryHandler(ITicketQueryRepository ticketQueryRepository) : IQueryHandler<ReadOneQuery, TicketDto>
 {
     public Task<TicketDto> HandleAsync(ReadOneQuery query, CancellationToken cancellationToken) 
-        => ticketQueryRepository.FindByIdByProjectionAsync(ticket => new TicketDto {
-            Id = ticket.Id,
-            Title = ticket.Title,
-            Description = ticket.Description,
-            Priority = ticket.Priority,
-            Status = ticket.Status,
-            Username = ticket.User.Username,
-            FirstName = ticket.User.FirstName,
-            LastName = ticket.User.LastName,
-            UserImage = ticket.User.UserImage,
-            CategoryName = ticket.Category.Title
-        }, query.Id, cancellationToken);
+        => ticketQueryRepository.FindByIdByProjectionConditionallyAsync(query.Id, 
+            ticket => new TicketDto {
+                Id = ticket.Id,
+                Title = ticket.Title,
+                Description = ticket.Description,
+                Priority = ticket.Priority,
+                Status = ticket.Status,
+                Username = ticket.User.Username,
+                FirstName = ticket.User.FirstName,
+                LastName = ticket.User.LastName,
+                UserImage = ticket.User.UserImage,
+                CategoryName = ticket.Category.Title
+            },
+            ticket => !string.IsNullOrEmpty(query.UserId) && ticket.User.Id == query.UserId,
+            cancellationToken
+        );
 }
