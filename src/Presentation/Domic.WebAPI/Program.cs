0ff965c3754737797e68d30b46ccb73b1b721be8
Hasan1375-tech <karami.samdad@gmail.com>
2025-05-02 17:46:11 +0330

    update

--- src/Presentation/Domic.WebAPI/Program.cs
+++ src/Presentation/Domic.WebAPI/Program.cs
@@ -30,6 +30,7 @@ builder.RegisterMessageBroker();
 builder.RegisterEventStreamBroker();
 builder.RegisterEventsSubscriber();
 builder.RegisterServices();
+builder.RegisterAssemblyTypesInMemory();
 
 builder.Services.AddMvc();
 builder.Services.AddHttpContextAccessor();
